1e9bb464b1895f6338e876cd0ad5ea2339a12e6f
Jeff Shively <jshively@paloaltonetworks.com>
2025-04-25 12:02:31 -0400

    Initial

---
+++ get_external_ip.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
+load_dotenv()
+
+ENV = os.environ.get("ENV")
+API_KEY = os.environ.get("API_KEY")
+
+
+url = f"https://api.{ENV}.datapath.prismaaccess.com/getPrismaAccessIP/v2"
+
+payload = json.dumps(
+    {"serviceType": "gp_gateway", "addrType": "active", "location": "all"}
+)
+headers = {"header-api-key": API_KEY, "Content-Type": "application/json"}
+
+response = requests.request("POST", url, headers=headers, data=payload).json()
+
+for response in response["result"]:
+    for address in response["address_details"]:
+        print(address["address"])
